7090ddc98ac80ff0750680634069fd2a9d908089
Charlie <cmainegra@protonmail.com>
2018-06-07 14:50:35 -0400

    near final commit

---
+++ game.js
@@ -0,0 +1,41 @@
+//declare variables
+var list = ["french", "english","spanish"];
+var listSelect = Math.floor(Math.random() * list.length);
+var word = list[listSelect];
+var underscore = [];
+var rightGuess = [];
+var wrongGuess = [];
+
+//DOM manipulation variables
+var underscoreText = document.getElementsByClassName("underscore");
+var rightGuessText = document.getElementsByClassName("right-guess");
+var wrongGuessText = document.getElementsByClassName("wrong-guess");
+
+//generate underscores
+function underscoreGenerate() {
+    for (let i = 0; i < word.length; i++) {
+        underscore.push("_");
+    }
+    return underscore;
+}
+// get userguess
+document.addEventListener("keypress", function (event) {
+    var keyGuess = String.fromCharCode(event.keyCode);
+    if (word.indexOf(keyGuess) > -1) {
+        rightGuess.push(keyGuess);
+        underscore[word.indexOf(keyGuess)] = keyGuess;
+        underscoreText[0].innerHTML = underscore.join(" ");
+        rightGuessText[0].innerHTML = rightGuess;
+        if (underscore.join("") == word) {
+            alert("You Win!")
+        }
+    }
+    else {
+        wrongGuess.push(keyGuess);
+        wrongGuessText[0].innerHTML = wrongGuess;
+        if (wrongGuess.length > 9) {
+            alert("You Lose");
+        }
+    }
+});
+underscoreText.innerHTML = underscoreGenerate().join(" ");
